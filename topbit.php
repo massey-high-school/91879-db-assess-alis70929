834dd783997456c76bdd827a6ecb43c69cfe867a
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-08-07 10:52:56 +1200

    Food name, restaurant and meal search finished, to do rating and vege/non vege

--- topbit.php
+++ topbit.php
@@ -29,7 +29,8 @@
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
     
     <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/FoodReviewStyle.css"> 
+    <link rel="stylesheet" href="css/FoodReviewStyle.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css"> 
     
 </head>
     
@@ -56,10 +57,60 @@
         
         <div class="box side">
            
-           <h2> <a href="index.php" class="side">Home</a>|<a href="showall.php" class="side">Show All</a></h2><br/>
-            Food Name<br/>
-            restauraunt<br/>
-            Meal<br/>
+           <h2> <a href="index.php" class="side">Home</a>|<a href="showall.php" class="side">Show All</a></h2><hr/>
+            
+            <!-- Search by Food Name -->
+            <form method = "post" action = "foodnamesearch.php" enctype="multipart/form-data">
+           
+           <input class = "search" type = "text" name = "foodname" size = "40" value = "" required placeholder = "Food Name..."/>
+            
+            <input class = "submit" type = "submit" name = "find_foodname" value = "&#xf002;" />
+           </form>
+            
+            <!-- End of Search by Restaurant Name -->
+            
+            <!-- Search by Food Name -->
+            <form method = "post" action = "restaurantsearch.php" enctype="multipart/form-data">
+           
+           <input class = "search" type = "text" name = "restaurant" size = "40" value = "" required placeholder = "Restaurant Name..."/>
+            
+            <input class = "submit" type = "submit" name = "find_restaurant" value = "&#xf002;" />
+           </form>
+            
+            <!-- End of Search by Restaurant Name -->
+            
+            <!-- Search By Meal -->
+           
+           <form method = "post" action = "mealsearch.php" enctype="multipart/form-data">
+           
+           <select name = "meal" required>
+               <option value = "" disabled selected>Meal...</option>
+               
+              <?php 
+               $meal_sql = "SELECT DISTINCT `Meal` FROM `2020_L1_Assesment_ShaAli` ORDER BY `2020_L1_Assesment_ShaAli`.`Meal` ASC LIMIT 0 , 30";
+               $meal_query = mysqli_query($dbconnect, $meal_sql); ;
+               $meal_rs = mysqli_fetch_assoc($meal_query) ;
+               
+               do{
+                   
+               ?>
+               
+               <option value = "<?php echo $meal_rs['Meal']; ?>"><?php echo $meal_rs['Meal']; ?></option>
+               
+               <?php
+               }// end of meal retrival
+               
+               while($meal_rs =mysqli_fetch_assoc($meal_query));
+               
+
+               ?>
+               
+            </select>
+            
+            <input class = "submit" type = "submit" name = "find_meal" value = "&#xf002;" />
+           </form>
+           
+           <!-- End of meal Search -->
             vege/nonvege<br/>
             Rating<br/>
         </div>
